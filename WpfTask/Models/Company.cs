012124a41d615988b9fa0cb2d3ea6c848183bec1
vasia98 <vasia.punk.rok@gmail.com>
2019-06-12 12:51:53 +0300

    added first version of company model

--- WpfTask/Models/Company.cs
+++ WpfTask/Models/Company.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WpfTask.Models
+﻿namespace WpfTask.Models
 {
     class Company
     {
+        #region Private fields
+
+        private int id;
+        private string name;
+        private string contructStatus;
+
+        #endregion
+
+        #region Constructor
+
+        public Company(int _id, string _name, string _contructStatus)
+        {
+            Id = _id;
+            Name = _name;
+            ContructStatus = _contructStatus;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public string ContructStatus
+        {
+            get => contructStatus;
+            set => contructStatus = value;
+        }
+        #endregion
     }
 }
