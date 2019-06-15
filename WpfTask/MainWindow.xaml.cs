ccfa0d6f3e957d95c6e3dc6ac1be002d89f848a2
vasia98 <vasia.punk.rok@gmail.com>
2019-06-15 14:57:47 +0300

    removed models

--- WpfTask/MainWindow.xaml.cs
+++ WpfTask/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Data.Entity;
 using System.Windows;
-using WpfTask.Models;
 
 namespace WpfTask
 {
@@ -8,43 +7,11 @@ namespace WpfTask
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        RiPContext db;   
+    {   
         public MainWindow()
         {
             InitializeComponent();
-
-            db = new RiPContext();
-            db.Companys.Load();
-            companysGrid.ItemsSource = db.Companys.Local.ToBindingList();
-
-            this.Closing += MainWindow_Closing;
         }
-
-        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            db.Dispose();
-        }
-
-        private void updateButton_Click(object sender, RoutedEventArgs e)
-        {
-            db.SaveChanges();
-        }
-
-        private void deleteButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (companysGrid.SelectedItems.Count > 0)
-            {
-                for (int i = 0; i < companysGrid.SelectedItems.Count; i++)
-                {
-                    var company = companysGrid.SelectedItems[i] as Company;
-                    if (company != null)
-                    {
-                        db.Companys.Remove(company);
-                    }
-                }
-            }
-            db.SaveChanges();
-        }
+        
     }
 }
