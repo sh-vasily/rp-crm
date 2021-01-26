d372cd0a54e20ed3dfc902fde1fec3b737251c3b
Василий Щербаков <v.sherbakov@softpro.com>
2021-01-26 20:43:43 +0300

    [TDD] first test for user repository

---
+++ RP-CRM.Tests/UsersRepositoryTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using RP_CRM.Data;
+using Xunit;
+
+namespace RP_CRM.Tests
+{
+    public class UsersRepositoryTests
+    {
+        private readonly GenericRepository<User> _usersRepositoryUnderTest;
+        private readonly List<User> _stubStorage;
+        private readonly DbSet<User> _stubSet;
+        
+        public UsersRepositoryTests()
+        {
+            var dbSetMock = new Mock<DbSet<User>>();
+
+            dbSetMock.Setup(dbSet => dbSet.Add(It.IsAny<User>()))
+                .Callback((User user) => _stubStorage.Add(user));
+            dbSetMock.Setup(dbSet => dbSet.Remove(It.IsAny<User>()))
+                .Callback((User user) => _stubStorage.Remove(user));
+            dbSetMock.Setup(dbSet => dbSet.Add(It.IsAny<User>()))
+                .Callback((User user) => _stubStorage.Add(user));
+
+            var dbContextMock = new Mock<DatabaseContext>();
+
+            dbContextMock.SetupProperty<DbSet<User>>(dbContext => dbContext.Users, dbSetMock.Object);
+            
+            _usersRepositoryUnderTest = new GenericRepository<User>(dbContextMock.Object);
+        }
+
+        [Fact]
+        public void UserShouldBeCorrectAddedToStorageWhenThereIsNoSuchUsers()
+        {
+            //arrange
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = "Fake User",
+                Login = "Fake Login",
+                Password = "Fake Password"
+            };
+            //act
+            _usersRepositoryUnderTest.AddOrUpdate(user);
+            //assert
+            _stubStorage
+                .Should()
+                .Contain(x => x.Id == user.Id &&
+                    x.Name == user.Name &&
+                    x.Login == user.Login &&
+                    x.Password == user.Password);
+        }
+    }
+}
