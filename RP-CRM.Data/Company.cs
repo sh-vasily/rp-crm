42a159add26246f7ee0584de62c03e509587209a
Василий Щербаков <v.sherbakov@softpro.com>
2021-01-26 21:26:04 +0300

    add ctor for company

--- RP-CRM.Data/Company.cs
+++ RP-CRM.Data/Company.cs
@@ -5,5 +5,12 @@ namespace RP_CRM.Data
     public class Company : BaseEntity
     {
         public ContractStatus ContractStatus { get; set; }
+
+        public Company(string companyName)
+        {
+            Id = Guid.NewGuid();
+            Name = companyName;
+            ContractStatus = ContractStatus.NotConcluded;
+        }
     }
 }
